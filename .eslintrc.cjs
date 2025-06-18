466e36259d2d6d6a3cd64e805d913a1794e6f8a0
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-06-18 08:20:28 -0400

    Fix merge conflicts and update lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
-export default {
+module.exports = {
   root: true,
   env: {
     browser: true,
     es2022: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'prettier'
@@ -16,25 +16,17 @@ export default {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
+    ecmaFeatures: { jsx: true }
   },
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  settings: {
-    react: {
-      version: '18.2'
-    }
-  },
+  settings: { react: { version: 'detect' } },
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react/prop-types': 'off'
   },
   ignorePatterns: ['dist/', 'node_modules/']
 };
