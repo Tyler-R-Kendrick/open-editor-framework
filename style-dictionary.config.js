df4c9c3b193f8cf54853bf9f82b73006187fcde1
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-06-18 08:00:59 -0400

    feat: add style-dictionary build

---
+++ style-dictionary.config.js
@@ -0,0 +1,15 @@
+export default {
+  source: ['tokens/**/*.json'],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'src/styles/',
+      files: [
+        {
+          destination: 'tokens.css',
+          format: 'css/variables'
+        }
+      ]
+    }
+  }
+};
