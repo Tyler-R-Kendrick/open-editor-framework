178e431dec27cb6a600b11d59083ea207086219c
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-06-18 15:33:36 +0000

    chore: update storybook configuration and dependencies
    - Updated storybook command to disable telemetry and added options for host, no-open, and CI mode.
    - Introduced new storybook:safe and storybook:debug scripts for production and verbose logging respectively.
    - Upgraded @chromatic-com/storybook to version 4.0.0 and @storybook/react-vite to version 9.0.12.
    - Updated storybook dependency to version 9.0.12.
    - Added eslint-plugin-storybook and @storybook/addon-docs to devDependencies.
    - Removed duplicate import of tokens.css in global.css.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import '../src/styles/global.css';
 
-/** @type { import('@storybook/web-components').Preview } */
+/** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
     controls: {
@@ -8,8 +9,13 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/
       }
-    }
-  }
+    },
+    layout: 'centered'
+  },
+  decorators: [
+    (Story) => React.createElement('div', { style: { margin: '3em' } }, React.createElement(Story))
+  ],
+  tags: ['autodocs']
 };
 
 export default preview;
